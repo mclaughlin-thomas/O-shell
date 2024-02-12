1e9a8a0b24ab59eeb02247c0db4e5a0512b477aa
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-02-12 01:45:45 -0500

    Update CircularArray.h

--- CircularArray.h
+++ CircularArray.h
@@ -1,1 +1,32 @@
 
+// CircularArray.h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef short bool;
+#define true  1
+#define false 0
+
+#define STRINGMAX  81   /* max number of chars in a string */
+#define MAXSTRINGS 10   /* max number of strings in the circular array */
+
+typedef char StringType[STRINGMAX];
+
+// Declare global variables with extern
+extern int HistoryFront;
+extern int HistoryRear;
+extern int HistoryCount;
+extern StringType HistoryData[MAXSTRINGS];
+
+void HistoryInit(void);
+void HistoryAdvance(int *IndexPtr);
+bool HistoryEmpty(void);
+bool HistoryFull(void);
+void HistoryInsert(StringType str);
+void HistoryLookup(int number, StringType str);
+
+#endif
